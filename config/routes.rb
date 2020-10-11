1ffb367907448a94e61ae7ced3e55b9906f639ae
helloimiskra <helloimiskra@gmail.com>
2020-10-12 06:13:47 +0900

    adds configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :moods
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users do 
+        resources :moods
+      end
+    end
+  end
 end
