bd2f495465c7f6b1e55c85dd31ee93d17bf97023
helloimiskra <helloimiskra@gmail.com>
2020-10-19 16:06:26 +0900

    adds basic CRUD functionality to moods controller

--- app/controllers/api/v1/moods_controller.rb
+++ app/controllers/api/v1/moods_controller.rb
@@ -1,22 +1,40 @@
 class Api::V1::MoodsController < ApplicationController
     
     def index
+        @moods = Mood.all
+        render json: @moods
 
     end
 
     def create
+        @mood = Mood.new(mood_params)
+        if @mood.save
+            render json: @mood
+        else
+            rendor json: {error: "Error creating mood"}
+        end
+
     end
 
     def show
+        @mood = Mood.find(params[:id])
+        render json: @mood
+    
     end
 
     def destroy
+        @mood = Mood.find(params[:id])
+        @mood.destroy
     end
 
     def edit
+        @mood = Mood.find(params[:id])
     end
 
     def update
+        @mood = Mood.find(params[:id])
+        @mood.update(mood_type: params[:mood][:mood_type], date: params[:mood][:date], notes: params[:mood][:notes])
+        render json: @mood
     end
 
     private
